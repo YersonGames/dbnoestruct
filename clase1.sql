c93e4ab3a34b7f2e8e7ea02e57988e61718e11e7
YersonGames <pirno98k@gmail.com>
2026-03-17 12:12:04 -0300

    a

---
+++ clase1.sql
@@ -0,0 +1,156 @@
+/*
+
+Que es un dato?
+
+Un dato es una representacion simbolica de un hecho, objeto o evento del mundo real.
+
+Los datos puedes ser:
+    -Letras
+    -Numeros
+    -Imagenes
+    -Videos
+
+De donde provienen los datos?
+
+How los datos provienen de muchas fuentes:
+    -Redes sociales
+    -Aplicaciones moviles
+    -Sensores IoT
+    -Sistemas empresariales
+    -Camaras
+    -Dispositivos moviles
+
+Practicamente todo genera datos
+
+Diferencia entre Datos e Informacion
+
+Datos:
+Valores sin interpretar: Pedro, 23, Temuco
+
+Informacion:
+Datos procesados con significado: Pedro es un estudiante de 23 años que vive en Temuco
+
+Que es una base de datos?
+
+Una base de datos es un conjunto organizado de datos almacenas de forma estructurada para facilitar:
+    -Almacenamiento
+    -Consultar
+    -Actualizar
+    -Analizar
+
+Las bases de datos permiten gestionar grandes volumenes de informacion
+
+Ejemplo de bases de datos
+
+----------------------------|
+ID | Nombre | Carrera       |
+1  | Ana    | Informatica   |
+2  | Pedro  | Administracion|
+3  | Luis   | Contabilidad  |
+----------------------------|
+
+Los datos se organizan en tablas, donde:
+    -Cada fila es un registro
+    -Cada columna es un atributo
+
+Sistema de gestion de bases de datos
+
+Un DBMS es el software que permite gestionar bases de datos
+
+Funciones principales:
+    -Crear
+    -Insertar
+    -Consultar
+    -Actualizar
+
+Ejemplos:
+
+    -SQL Server
+    -Oracle
+    -mongoDB
+    -PostgreSQL
+
+El crecimiento de los datos
+
+Actualmente vivimos en la era del Big Data
+
+Las empresas generar a gran escala:
+    -Redes sociales
+    -Videos
+    -Transaccion online
+    -Sensores
+    -Aplicaciones moviles
+
+Se estima que mas del 80% de los datos generados hoy son no estructuradas
+
+Tipos de datos
+
+Los datos puedes clasificarse en:
+    -Datos estructurados
+    -Datos semi-estructurados
+    -Datos no estructurados
+
+Datos Estructurados:
+    -Registros de clientes
+    -Sistemas bancarios
+    -Inventarios
+    -Sistema de ventas
+    -Bases de datos empresariales
+
+
+Datos semi estructurados
+
+No siguen estructura rigida, pero tienen etiquetas que organizan la informacion
+
+Ejemplos de formato:
+    -JSON
+    -HTML
+    -XML
+
+Datos no estructurados
+
+No tienen una estructura definica
+Ejemplos:
+    -Imagenes
+    -Videos
+    -Audios
+    -Correos electronicos
+    -Documentos PDF
+    -Publicaciones de redes sociales
+
+Problema de las bases de datos tradicionales
+
+Las bases de datos relaciones fueron diseñadas para:
+    -Datos estructurados
+    -Sistemas transaccionales
+
+Pero presentan dificultades con:
+    -Grandes volumenes de datos
+    -Datos no estructurados
+    -Aplicaciones web masivas
+
+Surgimiento de NoSQL
+
+Para enfrentar estos desafios surgieron las bases de datos NoSQL
+
+Significado -> Not Only SQL
+
+Caracteristicas:
+    -Escalabilidad
+    -Flexibilidad
+    -Manejo de grandes volumenes de datos
+
+Ejemplos de bases de datos NoSQL
+
+    -Redis
+    -Neo4j
+    -Firebase
+    -Cassandra
+
+Utilizadas por empresas como:
+    -Google
+    -Netflix
+    -Facebook
+    -Amazon
+
+*/
